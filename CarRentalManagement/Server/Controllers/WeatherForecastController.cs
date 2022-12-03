c21ac07d8d0d904d3c7aec1e90367c85a8871082
JASON PHUA KAI XIANG 2105200F <2105200F@student.tp.edu.sg>
2022-12-03 23:23:34 +0800

    Added REST API Endpoints and Unit Of Work pattern

--- CarRentalManagement/Server/Controllers/WeatherForecastController.cs
+++ CarRentalManagement/Server/Controllers/WeatherForecastController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CarRentalManagement.Shared;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization;	
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CarRentalManagement.Server.Controllers
 {
-	[Authorize]
+	//[Authorize]
 	[ApiController]
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
